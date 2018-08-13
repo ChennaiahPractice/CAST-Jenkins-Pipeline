3bb9a384dc6fafaa68260d53027b8e248fae0fab
prabinovich <pavel.rabinovich@gmail.com>
2018-08-13 18:04:33 -0400

    Removed extra space from snapshot version

--- analysis-test.bat
+++ analysis-test.bat
@@ -3,7 +3,7 @@
 set profile=webstore
 set app=Webstore
 set packageTemplate=Baseline
-set snapshotName=%date:~-4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2% 
+set snapshotName=%date:~-4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%
 
 echo -- Packaging and Delivery of Source Code --
 call .\\CLI-Scripts\\CMS_AutomateDelivery.bat "profile=%profile%" "app=%app%" "fromVersion=%packageTemplate%" "version=%snapshotName%"
